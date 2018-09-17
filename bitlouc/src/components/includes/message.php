30d43a94a0140734a316bceac5239ea08fedfaa4
fabiobonina <fabiobonina@gmail.com>
2018-09-17 06:56:28 -0300

    equi

--- bitlouc/src/components/includes/message.php
+++ bitlouc/src/components/includes/message.php
@@ -1,10 +1,10 @@
 <template id="message">
     <div>
-        <v-snackbar v-model="temErros" color="error" top multi-line :timeout="timeout">
+        <v-snackbar v-model="temErros" color="error" top multi-line :timeout="timeout" absolute>
             <p v-for="message in error">{{ message }} </p>
             <v-btn dark flat @click="temErros = false">Close</v-btn>
         </v-snackbar>
-        <v-snackbar v-model="temMessage" color="success" top multi-line :timeout="timeout">
+        <v-snackbar v-model="temMessage" color="success" top multi-line :timeout="timeout" absolute>
             <p v-for="message in success">{{ message }} </p>
             <v-btn dark flat @click="snackbar = false">Close</v-btn>
         </v-snackbar>
@@ -23,7 +23,7 @@
         return {
             snackbar: false,
             snackbar1: false,
-            timeout: 20000,
+            timeout: 40000,
         };
         },
         watch: {
